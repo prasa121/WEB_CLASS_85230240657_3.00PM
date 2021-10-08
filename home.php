e6be552d4ecbe4bdebc8cf6f5723b3b7f603e73a
prasa121 <senanayaka.prasanna@gmail.com>
2021-10-08 17:05:49 +0530

    2021-10-08 5.05PM

---
+++ home.php
@@ -0,0 +1,14 @@
+<?php
+
+$a = 10;
+$b = 20;
+
+?>
+
+<table border='1'>
+    <tr><td>Addition</td><td> <?php echo ($a+$b) ?></td></tr>
+    <tr><td>Subtraction</td><td> <?php echo ($a-$b) ?></td></tr>
+    <tr><td>Division</td><td> <?php echo ($a/$b) ?> </td></tr>
+    <tr><td>Multiplication</td><td> <?php echo ($a*$b) ?> </td></tr>  
+</table>
+
---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+    
+    
+    <?php
+    
+    $hello = 10;
+    
+    echo $hello;
+    
+    ?>
+    
+    
+    
+    
+</html>
